4bd2b48101fb00a5827da54dc3487abdd40b9787
Erick Macedo Pinto <erimacedo_92@hotmail.com>
2021-07-21 09:04:45 -0300

    Create Domain.Service project

---
+++ src/BiomedSympCertificate.Domain.Service/Services/SpreadsheetReaderService.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.IO;
+using BiomedSympCertificate.Domain.Model.Interfaces;
+using ExcelDataReader;
+
+namespace BiomedSympCertificate.Domain.Service.Services
+{
+    public class SpreadsheetReaderService : ISpreadsheetReaderService
+    {
+        public DataSet Read(
+            string filePath)
+        {
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    return reader.AsDataSet(new ExcelDataSetConfiguration
+                    {
+                        ConfigureDataTable = tableReader => new ExcelDataTableConfiguration { UseHeaderRow = true }
+                    });
+                }
+            }
+        }
+    }
+}
---
+++ src/BiomedSympCertificate.Domain.Service/Services/SubscriberService.cs
@@ -0,0 +1,42 @@
+﻿using BiomedSympCertificate.Domain.Model.Entities;
+using BiomedSympCertificate.Domain.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace BiomedSympCertificate.Domain.Service.Services
+{
+    public class SubscriberService : ISubscriberService
+    {
+        public ICollection<Subscriber> CreateSubscribers(
+            DataSet spreadsheet)
+        {
+            var subscribers = new List<Subscriber>();
+
+            foreach (DataRow row in spreadsheet.Tables[0].Rows)
+            {
+                var signDateTime = Convert.ToDateTime(row[0]);
+                var name = row[1].ToString().Trim();
+                var email = row[2].ToString().Trim();
+                var registration = row[3].ToString().Trim();
+
+                subscribers.Add(new Subscriber(signDateTime, name, email, registration));
+            }
+
+            return subscribers;
+        }
+
+        public IEnumerable<Subscriber> GetDistinctSubscribers(
+            ICollection<Subscriber> subscribers,
+            int minimumCount)
+        {
+            return subscribers
+                .GroupBy(x => x.Email)
+                .Where(x => x.ToList().Count >= minimumCount)
+                .Select(x => x.ToList())
+                .Select(x => x[0])
+                .ToList();
+        }
+    }
+}
